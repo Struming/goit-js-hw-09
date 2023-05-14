075090ead0a74c1ae193099ba17143169e00d4e9
Struming <112948432+Struming@users.noreply.github.com>
2023-05-14 11:42:20 +0000

    Deploying to gh-pages from @ Struming/goit-js-hw-09@1dbe54b884524adc1ef4f1dd2a25aa3b4245467c 🚀

---
+++ 01-color-switcher.5d11a204.js
@@ -0,0 +1,2 @@
+!function(){var t,e=document.querySelector("[data-start]"),a=document.querySelector("[data-stop]");function d(){var t=document.body,e="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,"0"));t.style.backgroundColor=e}e.addEventListener("click",(function(){e.disabled=!0,a.disabled=!1,t=setInterval(d,1e3)})),a.addEventListener("click",(function(){e.disabled=!1,a.disabled=!0,clearInterval(t)}))}();
+//# sourceMappingURL=01-color-switcher.5d11a204.js.map
